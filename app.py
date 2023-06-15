e09897f27c1ae013d6a045df981e188bcbfa15c7
Freddy San <falexandersandoval@gmail.com>
2023-06-15 22:08:04 +1000

    added html contdown template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello world once again!"
+    return render_template("countdown.html")
 
 
 if __name__ == "__main__":
