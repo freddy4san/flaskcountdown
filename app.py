2a039432c572562ea1e0cd25e746746f09166ada
Freddy San <falexandersandoval@gmail.com>
2023-07-06 19:42:11 +1000

    general modifications to the api rest redirection of pages and style of the message shown in the flame screen

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from datetime import datetime 
 
 app = Flask(__name__)
 
+#secret for flash messaged to work
+#app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 @app.route("/")
 def home():
-    return render_template("countdown.html")
+    
+    countdowndate = datetime(2023,7,17)
+    todaysdate = datetime.now()
+    print(countdowndate)
+    print(todaysdate)
+    print(countdowndate > todaysdate)
+    #if countdown has not get to zero then keep showing it
+    if(countdowndate > todaysdate):
+        return render_template("countdown.html")
+    else:
+        #when countdown get to zero then redirect to different page
+        return redirect(url_for('generateflame'))
 
 @app.route("/flame")
-def generate():
+def generateflame():
+    #output = "Hello Diablita, If you are reading this it means countdown has come to zero. Now you get +1 level at your wise and strenght"
+    #bdmessage = "Happy birthday to the darkest star in the universe."
+    #flash("Diablita message going through")
+    return render_template("index.html")
 
-    output = "Hello Diablita, If you are reading this it means countdown has come to zero. Now you get +1 level at your wise and strenght"
-    bdmessage = "Happy birthday to the darkest star in the universe."
-
-    return render_template("index.html", output=output, bdmessage=bdmessage)
+@app.route('/message', methods=['GET', 'POST'])
+def delivermessage():
+    # GET request
+    if request.method == 'GET':
+        hellomessage = 'Hello Diablita, Looks like the countdown has come to zero. Now you get +1 level at your wise and strenght. Happy birthday, you are the coolest person in the universe!'        
+        return hellomessage
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -3,15 +3,25 @@ let flameSize = 4;
 function myFunction() {
   const flame = document.querySelector('.flame');
   console.log(flame);
-  flameSize = ++flameSize;
-  flame.style.setProperty(
-    'transform',
-    `scale(${flameSize})
-    translate(-13px, -15px)`,
-  );
+  if(flameSize < 12){
+    flameSize = ++flameSize;
+    flame.style.setProperty(
+      'transform',
+      `scale(${flameSize})
+      translate(-13px, -15px)`,
+    );
+  }
 
-  if(flameSize > 12){
-    console.log("in flames.....");    
+  if(flameSize == 10){
+    console.log("in flames.....");
+    const textelement = document.getElementById("flashtext");
+
+    fetch('/message')
+    .then(function (response) {
+        return response.text ();
+    }).then(function (text) {
+        textelement.innerHTML = '<span class="type" style="--n:200">'+text+'</span>';
+    });    
   }
 }
 
